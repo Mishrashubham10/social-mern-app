2c17b29aef35a2c3c2ff224b66e89b6562e48a79
Shubam Mishra <shubhammishra10101998@gmail.com>
2023-11-12 21:34:53 +0530

    Navbar and Sidebar added

--- frontend/src/components/leftBar/LeftBar.jsx
+++ frontend/src/components/leftBar/LeftBar.jsx
@@ -1,8 +1,90 @@
 import './leftbar.scss'
+import Friends from '/1.png'
+import Groups from '/2.png'
+import Market from '/3.png'
+import Watch from '/4.png'
+import Memories from '/5.png'
+import Events from '/6.png'
+import Gaming from '/7.png'
+import Gallery from '/8.png'
+import Videos from '/9.png'
+import Messages from '/10.png'
+import Tutorials from '/11.png'
+import Courses from '/12.png'
+import Fund from '/13.png'
 
 const LeftBar = () => {
   return (
-    <div>LeftBar</div>
+    <div className='leftBar'>
+      <div className="container">
+        <div className="menu">
+          <div className="user">
+            <img src="https://images.pexels.com/photos/19065473/pexels-photo-19065473/free-photo-of-a-view-of-the-blue-mosque-from-a-window.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Img" />
+            <span>Shubham Mishra</span>
+          </div>
+          <div className="item">
+            <img src={Friends} alt="" />
+            <span>Friends</span>
+          </div>
+          <div className="item">
+            <img src={Groups} alt="" />
+            <span>Groups</span>
+          </div>
+          <div className="item">
+            <img src={Market} alt="" />
+            <span>Marketplace</span>
+          </div>
+          <div className="item">
+            <img src={Watch} alt="" />
+            <span>Watch</span>
+          </div>
+          <div className="item">
+            <img src={Memories} alt="" />
+            <span>Memories</span>
+          </div>
+        </div>
+        <hr />
+        <div className="menu">
+          <span>Your shortcuts</span>
+          <div className="item">
+            <img src={Events} alt="" />
+            <span>Events</span>
+          </div>
+          <div className="item">
+            <img src={Gaming} alt="" />
+            <span>Gaming</span>
+          </div>
+          <div className="item">
+            <img src={Gallery} alt="" />
+            <span>Gallery</span>
+          </div>
+          <div className="item">
+            <img src={Videos} alt="" />
+            <span>Videos</span>
+          </div>
+          <div className="item">
+            <img src={Messages} alt="" />
+            <span>Messages</span>
+          </div>
+        </div>
+        <hr />
+        <div className="menu">
+          <span>Others</span>
+          <div className="item">
+            <img src={Fund} alt="Fund" />
+            <span>Fundraiser</span>
+          </div>
+          <div className="item">
+            <img src={Tutorials} alt="Tutorials" />
+            <span>Tutorials</span>
+          </div>
+          <div className="item">
+            <img src={Courses} alt="Courses" />
+            <span>Courses</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/rightBar/RightBar.jsx
+++ frontend/src/components/rightBar/RightBar.jsx
@@ -2,7 +2,33 @@ import './rightbar.scss'
 
 const RightBar = () => {
   return (
-    <div>RightBar</div>
+    <div className='rightBar'>
+      <div className="container">
+        <div className="item">
+          <span>Suggestions for you</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/18928507/pexels-photo-18928507/free-photo-of-fireworks-in-the-dark-sky-with-a-black-background.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="User" />
+              <span>Shubham Mishra</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/18928507/pexels-photo-18928507/free-photo-of-fireworks-in-the-dark-sky-with-a-black-background.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="User" />
+              <span>Shubham Mishra</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
